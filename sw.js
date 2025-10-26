e41bceef443814403145ff73aa74449c545089ab
wesleyhson <merkabarecords@protonmail.com>
2025-10-25 22:37:48 -0500

    update vibes

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'vibraphonics-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/dropzone/test-applet.html'
+  // Add more applets here as you grow!
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
